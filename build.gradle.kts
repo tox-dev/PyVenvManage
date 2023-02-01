e232d3eac0bcd72332ac2d5fa5a973710c8a1155
Bernát Gábor <bgabor8@bloomberg.net>
2023-01-31 16:37:01 -0800

    Allow running on 2023.1
    Signed-off-by: Bernát Gábor <bgabor8@bloomberg.net>

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,18 +5,18 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     id("java")
-    id("org.jetbrains.kotlin.jvm") version "1.7.10"
-    id("org.jetbrains.intellij") version "1.9.0"
+    id("org.jetbrains.kotlin.jvm") version "1.8.0"
+    id("org.jetbrains.intellij") version "1.12.0"
     id("org.jetbrains.changelog") version "1.3.1"
     id("io.gitlab.arturbosch.detekt") version "1.22.0"
-    id("org.jlleitschuh.gradle.ktlint") version "11.0.0"
+    id("org.jlleitschuh.gradle.ktlint") version "11.1.0"
 }
 
 val pluginGroup: String = "com.github.pyvenvmanage.pyvenv"
 val pluginNameG: String = "PyVenv Manage"
-val pluginVersion: String = "1.3.7"
+val pluginVersion: String = "1.3.8"
 val pluginSinceBuild = "211"
-val pluginUntilBuild = "223.*"
+val pluginUntilBuild = "231.*"
 // https://www.jetbrains.com/idea/download/other.html
 val pluginVerifierIdeVersions = "222.4345.14"
 val platformType = "IC"
